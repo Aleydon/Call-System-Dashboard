90e6f67be32a5fa9bb3e602364d15a7b10b66ffe
Aleydon <aleydonroberto@gmail.com>
2021-05-06 17:45:17 -0300

    Create cadaster page with style

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,54 @@
-import './style';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import logo from '../../assets/logo.png';
+import { Container, LogoContainer, Form, Login } from './style';
 
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  function handleCadaster(e) {
+    e.preventDefault();
+    console.log('hueue');
+  }
+
   return (
-    <>
-      <h3>SignUp</h3>
-    </>
+    <Container>
+      <Login>
+        <LogoContainer>
+          <img src={logo} alt="logo" />
+        </LogoContainer>
+
+        <Form onSubmit={handleCadaster}>
+          <h2>Cadaster</h2>
+
+          <input
+            type="text"
+            placeholder="Your name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <input
+            type="email"
+            placeholder="email@email.com"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="********"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <br />
+          <button type="submit">Create account</button>
+        </Form>
+
+        <a href="/">Already have an account</a>
+      </Login>
+    </Container>
   );
 }
 
---
+++ src/pages/SignUp/style.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #121212;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #181c2e;
+  margin-bottom: 3px;
+
+  img {
+    width: 600px;
+    height: 200px;
+    padding: 30px;
+  }
+`;
+
+export const Login = styled.div`
+  background-color: #eaeaec;
+  width: 600px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  width: 90%;
+  flex-direction: column;
+
+  h2 {
+    text-align: center;
+    margin: 5px 0;
+  }
+
+  input {
+    margin: 7px;
+    height: 30px;
+    border: 1px solid gray;
+    border-radius: 7px;
+    padding: 6px;
+  }
+
+  button {
+    margin: 7px;
+    height: 30px;
+    border: 1px solid gray;
+    border-radius: 7px;
+    padding: 6px;
+    font-weight: bold;
+  }
+
+  button:hover {
+    background-color: #181c2e;
+    color: #fff;
+  }
+`;
