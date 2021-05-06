1edc82d0f9a28cc5946927051d27ce84ba754bba
Aleydon <aleydonroberto@gmail.com>
2021-05-06 17:33:15 -0300

    Created login page with style

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './style';
+import logo from '../../assets/logo.png';
+import { Container, LogoContainer, Form, Login } from './style';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handlelogin(e) {
+    e.preventDefault();
+    console.log('hueue');
+  }
+
   return (
     <Container>
-      <h3>SignIn</h3>
+      <Login>
+        <LogoContainer>
+          <img src={logo} alt="logo" />
+        </LogoContainer>
+
+        <Form onSubmit={handlelogin}>
+          <h2>Login</h2>
+          <input
+            type="email"
+            placeholder="email@email.com"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="********"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <br />
+          <button type="submit">Acess</button>
+        </Form>
+
+        <a href="/">Create an account</a>
+      </Login>
     </Container>
   );
 }
