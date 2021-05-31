2475baf1af59c9d9cc74829ff9be682931c50afd
Aleydon <aleydonroberto@gmail.com>
2021-05-31 07:31:25 -0300

    Set create an account context

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -21,8 +21,62 @@ function AuthProvider({ children }) {
     loadStorage();
   }, []);
 
+  function storageUserSave(data) {
+    localStorage.setItem('@loggedUser', JSON.stringify(data));
+  }
+
+  async function signUp(email, password, name) {
+    setLoading(true);
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        const { uid } = value.user;
+        await firebase
+          .firestore()
+          .collection('users')
+          .doc(uid)
+          .set({ name, avatarUrl: null })
+          .then(() => {
+            const data = {
+              uid,
+              name,
+              email: value.user.email,
+              avatarUrl: null
+            };
+            setUser(data);
+            storageUserSave(data);
+            setLoading(false);
+          })
+          .catch((err) => console.log(`SignUp error: ${err}`));
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }
+
+  async function signIn(email, password) {
+    setLoadingAuth(true);
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(async (value) => console.log(value))
+      .catch((err) => console.log(err));
+  }
+
+  async function signOut() {
+    await firebase
+      .auth()
+      .signOut()
+      .then((resolve) => console.log(`SignOut sucessful ${resolve}`))
+      .catch((err) => console.log(`SignOut error ${err}`));
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, loading }}>
+    <AuthContext.Provider
+      value={{ signed: !!user, user, loading, signIn, signUp, signOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
