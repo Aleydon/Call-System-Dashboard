dca184d9305d4f908a64dbcb68448408d2b27b2a
Aleydon <aleydonroberto@gmail.com>
2021-06-05 03:00:46 -0300

    Added logout button

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+import React, { useContext } from 'react';
+
+import { AuthContext } from '../../contexts/Auth';
 
 function Dashboard() {
+  const { signOut } = useContext(AuthContext);
+
+  function signOutAplicattion() {
+    signOut();
+  }
   return (
     <>
       <h3>Dashboard</h3>
+      <button onClick={signOutAplicattion}>Sign Out</button>
     </>
   );
 }
