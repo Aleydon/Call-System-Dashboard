974516a81930d1fd9ea9f93187bdfd554d53613a
Aleydon <aleydonroberto@gmail.com>
2021-06-19 10:30:00 -0300

    Created Profile/Settings page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import { FiSettings } from 'react-icons/fi';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState, useContext } from 'react';
+import { FiSettings, FiUpload } from 'react-icons/fi';
 
+import Avatar from '../../assets/avatar.png';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { Content } from './style';
+import { AuthContext } from '../../contexts/Auth';
+import { Content, Container, Form } from './style';
 
 function Profile() {
+  const { user } = useContext(AuthContext);
+  const [name, setName] = useState(user && user.name);
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+  const [email, setEmail] = useState(user && user.email);
+
   return (
     <>
       <Header />
@@ -13,6 +21,28 @@ function Profile() {
         <Title name="Profile">
           <FiSettings color="#000" size={25} />
         </Title>
+        <Container>
+          <Form>
+            <label htmlFor="avatar">
+              <span>
+                <FiUpload color="#f00" size={30} />
+              </span>
+              <input type="file" id="avatar" accept="image/*" />
+              <br />
+              {avatarUrl === null ? (
+                <img src={Avatar} alt="avatar" width={150} />
+              ) : (
+                <img src={avatarUrl} alt="avatar" width={150} />
+              )}
+            </label>
+
+            <label>Name:</label>
+            <input type="text" value={name} onChange={(e) => {}} />
+
+            <label>Email: </label>
+            <input type="text" value={email} onChange={(e) => {}} readOnly />
+          </Form>
+        </Container>
       </Content>
     </>
   );
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -4,3 +4,11 @@ export const Content = styled.div`
   margin-left: 200px;
   padding: 1px 16px;
 `;
+
+export const Container = styled.div`
+  margin-left: 15px;
+  background-color: #f8f8f8;
+  width: 96%;
+`;
+
+export const Form = styled.form``;
