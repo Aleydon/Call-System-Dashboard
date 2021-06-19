12b9c40e119f1fd5387ae8b12156159c6823a2a3
Aleydon <aleydonroberto@gmail.com>
2021-06-19 09:04:54 -0300

    Ajusted style

--- src/components/Title/style.js
+++ src/components/Title/style.js
@@ -4,13 +4,13 @@ export const TitleContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin: 1em;
   border-radius: 5px;
+  margin: 1em;
   background-color: #f8f8f8;
   padding: 10px 20px;
 
   span {
     margin-left: 1em;
-    font-size: 16px;
+    font-size: 1.5em;
   }
 `;
