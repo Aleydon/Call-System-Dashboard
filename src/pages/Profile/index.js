066451416bf070fc93ad849b70024dc2d3f05fbe
Aleydon <aleydonroberto@gmail.com>
2021-06-19 09:05:47 -0300

    Ajusted Icon/Title and Header in Profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { FiSettings } from 'react-icons/fi';
+
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { Content } from './style';
 
 function Profile() {
   return (
     <>
-      <h3>Profile</h3>
+      <Header />
+      <Content>
+        <Title name="Profile">
+          <FiSettings color="#000" size={25} />
+        </Title>
+      </Content>
     </>
   );
 }
---
+++ src/pages/Profile/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  margin-left: 200px;
+  padding: 1px 16px;
+`;
