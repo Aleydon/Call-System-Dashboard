6165c5a100d71b4317fbfd050ee61e3e82b34490
Aleydon <aleydonroberto@gmail.com>
2021-06-21 07:25:17 -0300

    Create firebase Image upload

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,3 +1,7 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-alert */
+/* eslint-disable react/button-has-type */
+/* eslint-disable import/named */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useContext } from 'react';
 import { FiSettings, FiUpload } from 'react-icons/fi';
@@ -6,13 +10,90 @@ import Avatar from '../../assets/avatar.png';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { AuthContext } from '../../contexts/Auth';
-import { Content, Container, Form } from './style';
+import firebase from '../../services/firebaseConnection';
+import {
+  Content,
+  Container,
+  Form,
+  LabelAvatar,
+  FormLabel,
+  LogoutContainer
+} from './style';
 
 function Profile() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut, setUser, storageUserSave } = useContext(AuthContext);
   const [name, setName] = useState(user && user.name);
   const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
   const [email, setEmail] = useState(user && user.email);
+  const [profileImagePhoto, setProfileImagePhoto] = useState(null);
+
+  function handleImage(e) {
+    if (e.target.files[0]) {
+      const image = e.target.files[0];
+
+      if (image.type === 'image/jpeg' || image.type === 'image/png') {
+        setProfileImagePhoto(image);
+        setAvatarUrl(URL.createObjectURL(e.target.files[0]));
+      } else {
+        alert('Select a photo');
+        setProfileImagePhoto(null);
+        return null;
+      }
+    }
+  }
+
+  async function handleImageUpload() {
+    const currentUID = user.uid;
+    await firebase
+      .storage()
+      .ref(`images/${currentUID}/${profileImagePhoto.name}`)
+      .put(profileImagePhoto)
+      .then(async () => {
+        await firebase
+          .storage()
+          .ref(`images/${currentUID}`)
+          .child(profileImagePhoto.name)
+          .getDownloadURL()
+          .then(async (url) => {
+            const imageURL = url;
+            await firebase
+              .firestore()
+              .collection('users')
+              .doc(user.uid)
+              .update({ avatarUrl: imageURL, name })
+              .then(() => {
+                const data = {
+                  ...user,
+                  avatarUrl: url,
+                  name
+                };
+                setUser(data);
+                storageUserSave(data);
+              });
+          });
+      });
+  }
+
+  async function handleSave(e) {
+    e.preventDefault();
+    if (profileImagePhoto === null && name !== '') {
+      await firebase
+        .firestore()
+        .collection('users')
+        .doc(user.uid)
+        .update({ name })
+        .then(() => {
+          const data = {
+            ...user
+          };
+          setUser(data);
+          storageUserSave(data);
+        })
+        .catch((error) => console.log(`Update profile error ${error}`));
+    } else if (name !== '' && profileImagePhoto !== null) {
+      handleImageUpload();
+    }
+  }
 
   return (
     <>
@@ -22,26 +103,50 @@ function Profile() {
           <FiSettings color="#000" size={25} />
         </Title>
         <Container>
-          <Form>
-            <label htmlFor="avatar">
+          <Form onSubmit={handleSave}>
+            <LabelAvatar htmlFor="avatar">
               <span>
-                <FiUpload color="#f00" size={30} />
+                <FiUpload color="#fff" size={30} />
               </span>
-              <input type="file" id="avatar" accept="image/*" />
+              <input
+                type="file"
+                id="avatar"
+                accept="image/*"
+                onChange={handleImage}
+              />
               <br />
               {avatarUrl === null ? (
                 <img src={Avatar} alt="avatar" width={150} />
               ) : (
                 <img src={avatarUrl} alt="avatar" width={150} />
               )}
-            </label>
+            </LabelAvatar>
 
-            <label>Name:</label>
-            <input type="text" value={name} onChange={(e) => {}} />
+            <FormLabel>
+              <label>Name:</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
 
-            <label>Email: </label>
-            <input type="text" value={email} onChange={(e) => {}} readOnly />
+              <label>Email: </label>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => {}}
+                disabled
+                style={{ cursor: 'not-allowed' }}
+              />
+              <button type="submit">Save</button>
+            </FormLabel>
           </Form>
+
+          <LogoutContainer>
+            <button onClick={() => signOut()}>Logout</button>
+          </LogoutContainer>
         </Container>
       </Content>
     </>
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -9,6 +9,91 @@ export const Container = styled.div`
   margin-left: 15px;
   background-color: #f8f8f8;
   width: 96%;
+  padding: 0.8em;
 `;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 3px;
+
+  button {
+    background-color: #092532;
+    color: #fff;
+    height: 25px;
+  }
+
+  button:hover {
+    background-color: #184d47;
+  }
+`;
+
+export const LabelAvatar = styled.label`
+  display: flex;
+  width: 180px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  cursor: pointer;
+
+  input {
+    display: none;
+  }
+
+  span {
+    z-index: 99;
+    opacity: 0.6;
+    position: absolute;
+    transition: all 0.3s;
+  }
+
+  span:hover {
+    opacity: 1;
+    transform: scale(1.2);
+  }
+
+  img {
+    margin-bottom: 6px;
+    border-radius: 5px;
+    object-fit: cover;
+    width: 150px;
+  }
+`;
+
+export const FormLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  padding: 0.7em;
+
+  input {
+    margin-bottom: 1em;
+    border: 0;
+    border-radius: 5px;
+    max-width: 600px;
+  }
+  button {
+    max-width: 600px;
+    margin-top: 5px;
+  }
+`;
+
+export const LogoutContainer = styled.div`
+  background-color: transparent;
+  padding: 8px 20px;
+  border: 1px solid #121212;
+  font-size: 1.5em;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  border: none;
+
+  button {
+    color: #fff;
+    background-color: #f00;
+    width: 100px;
+    border-radius: 3px;
+    outline: 0;
+    border: 0;
+    padding: 1px;
+  }
+`;
