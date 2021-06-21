88702997e8ad6c7e23a218b9f8d8b3f0b42aea40
Aleydon <aleydonroberto@gmail.com>
2021-06-21 18:21:42 -0300

    Set Form and style

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 import { FiUser } from 'react-icons/fi';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { Container, Content } from './style';
+import { Container, Content, Form, FormLabel } from './style';
 
 function Customers() {
+  const [businessInput, setBusinessInput] = useState('');
+  const [CNPJ, setCNPJ] = useState('');
+  const [address, setAddress] = useState('');
+
+  function cadasterCustomers(e) {
+    e.preventDefault();
+    alert('test');
+  }
+
   return (
     <>
       <Header />
@@ -13,7 +23,35 @@ function Customers() {
         <Title name="Customers">
           <FiUser size={25} color="#000" />
         </Title>
-        <Content />
+        <Content>
+          <Form onSubmit={cadasterCustomers}>
+            <FormLabel>
+              <label>Business:</label>
+              <input
+                type="text"
+                placeholder="business"
+                value={businessInput}
+                onChange={(e) => setBusinessInput(e.target.value)}
+              />
+              <label>CNPJ:</label>
+              <input
+                type="text"
+                placeholder="CNPJ"
+                value={CNPJ}
+                onChange={(e) => setCNPJ(e.target.value)}
+              />
+              <label>Address:</label>
+              <input
+                type="text"
+                placeholder="address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+
+              <button type="submit">Cadaster</button>
+            </FormLabel>
+          </Form>
+        </Content>
       </Container>
     </>
   );
--- src/pages/Customers/style.js
+++ src/pages/Customers/style.js
@@ -9,4 +9,49 @@ export const Content = styled.div`
   margin-left: 15px;
   background-color: #f8f8f8;
   width: 96%;
+  padding: 0.8em;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 3px;
+
+  button {
+    background-color: #092532;
+    color: #fff;
+    height: 25px;
+    width: 250px;
+  }
+
+  button:hover {
+    background-color: #184d47;
+  }
+
+  input {
+    width: 300px;
+    height: 20px;
+    padding: 5px;
+    margin-top: 5px;
+    border: 0;
+    border-radius: 5px;
+  }
+`;
+
+export const FormLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+  padding: 0.7em;
+
+  input {
+    margin-bottom: 1em;
+    border: 0;
+    border-radius: 5px;
+    max-width: 600px;
+  }
+  button {
+    max-width: 600px;
+    margin-top: 5px;
+    width: 300px;
+  }
 `;
