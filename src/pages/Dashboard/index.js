ac71e855b55eee1d25d462246b1b281a5080cb98
Aleydon <aleydonroberto@gmail.com>
2021-06-21 21:37:10 -0300

    Set Dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/button-has-type */
-import React, { useContext } from 'react';
-import { FiHome } from 'react-icons/fi';
+import React, { useContext, useState } from 'react';
+import { FiHome, FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
@@ -8,11 +9,8 @@ import { AuthContext } from '../../contexts/Auth';
 import { Container, Content } from './style';
 
 function Dashboard() {
-  const { signOut } = useContext(AuthContext);
+  const [calls, setCalls] = useState([]);
 
-  function signOutAplicattion() {
-    signOut();
-  }
   return (
     <>
       <Header />
@@ -20,7 +18,20 @@ function Dashboard() {
         <Title name="Dashboard">
           <FiHome color="#000" size={25} />
         </Title>
-        <Content />
+
+        {calls.length === 0 ? (
+          <Content>
+            <span>No calls</span>
+            <Link to="/new">
+              <FiPlus size={30} color="#fff" />
+              New Call
+            </Link>
+          </Content>
+        ) : (
+          <>
+            <Link to="/profile">Settings</Link>
+          </>
+        )}
       </Container>
     </>
   );
--- src/pages/Dashboard/style.js
+++ src/pages/Dashboard/style.js
@@ -8,4 +8,39 @@ export const Content = styled.div`
   margin-left: 15px;
   background-color: #f8f8f8;
   width: 96%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  span {
+    margin: 2em 0;
+    font-weight: 600;
+    font-size: 1.2em;
+  }
+
+  a {
+    float: right;
+    margin-bottom: 1.2em;
+    background-color: #83bf02;
+    color: #fff;
+    border: 0;
+    padding: 0.3em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 600;
+    transition: 0.9ms;
+    border-radius: 5px;
+  }
+
+  a:hover {
+    background-color: #5fd204;
+    transform: scale(1.1);
+  }
+
+  svg {
+    margin-right: 5px;
+    margin-left: 5px;
+  }
 `;
