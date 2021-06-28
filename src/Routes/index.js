dcb1bacb72c0a6674832d70f37d64e00616b481c
Aleydon <aleydonroberto@gmail.com>
2021-06-28 10:18:25 -0300

    Added New page

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 
 import Customers from '../pages/Customers';
 import Dashboard from '../pages/Dashboard';
+import New from '../pages/New';
 import Profile from '../pages/Profile';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -27,6 +28,7 @@ const Routes = () => {
           component={Customers}
           isPrivate
         />
+        <RoutesWrapper exact path="/new" component={New} isPrivate />
       </Switch>
     </>
   );
