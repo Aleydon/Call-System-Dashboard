f0679fdd28c005676feb086d1b13143a0807315f
Aleydon <aleydonroberto@gmail.com>
2021-06-29 14:16:16 -0300

    Set Toast Notification

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import AuthProvider from './contexts/Auth';
 import Routes from './Routes';
@@ -9,6 +10,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes />
+        <ToastContainer />
       </BrowserRouter>
     </AuthProvider>
   );
