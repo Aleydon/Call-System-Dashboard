8f27863b0b60bc9f325d57c042612d01afbeca0c
Aleydon <aleydonroberto@gmail.com>
2021-07-04 15:55:01 -0300

    Inserted inputs inside the label and added htmlFor

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,16 +1,71 @@
-/* eslint-disable no-alert */
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { FiPlusCircle } from 'react-icons/fi';
+import { toast } from 'react-toastify';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import { AuthContext } from '../../contexts/Auth';
+import firebase from '../../services/firebaseConnection';
 import { Container, Content, Form, FormLabel, Status } from './style';
 
 function New() {
+  const { user } = useContext(AuthContext);
   const [about, setAbout] = useState('Support');
   const [statusRadioButton, setStatusRadioButton] = useState('open');
   const [complement, setComplement] = useState('');
+  const [customerSelected, setCustomerSelected] = useState();
+  const [customers, setCustomers] = useState([]);
+  const [loadCustomers, setLoadCustomers] = useState(false);
+
+  useEffect(() => {
+    (async function loadAllCustomers() {
+      await firebase
+        .firestore()
+        .collection('customers')
+        .get()
+        .then((value) => {
+          const customersList = [];
+
+          value.forEach((doc) => {
+            customersList.push({
+              id: doc.id,
+              nameFantasy: doc.data().businessName
+            });
+          });
+          if (customersList.length === 0) {
+            toast.error('Sorry No companies found', {
+              position: 'top-right',
+              autoClose: 2500,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: false,
+              draggable: true,
+              progress: undefined
+            });
+            setLoadCustomers(false);
+            setCustomers([{ id: 0, nameFantasy: 'Sem Clientes' }]);
+          }
+          setCustomers(customersList);
+          setLoadCustomers(false);
+          console.log(customers);
+        })
+        .catch((err) => {
+          setCustomers([{ id: 0, nameFantasy: 'Sem Clientes' }]);
+          setLoadCustomers(false);
+          toast.error(`Customers Error ${err}`, {
+            position: 'top-right',
+            autoClose: 2500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined
+          });
+        });
+    })();
+  }, []);
+
+  function handleChangeCustomers() {}
 
   function handleAboutSelect(e) {
     setAbout(e.target.value);
@@ -32,28 +87,31 @@ function New() {
         <Content>
           <Form>
             <FormLabel>
-              <label>Customers:</label>
-              <select>
-                <option value="1" key={1}>
-                  Aleydon
-                </option>
-                <option value="2" key={2}>
-                  Fulaninho
-                </option>
-              </select>
+              <label htmlFor="customers">
+                Customers:
+                <select
+                  value={customerSelected}
+                  onChange={handleChangeCustomers}
+                  id="customers"
+                >
+                  {customers.map((customer, index) => {
+                    return (
+                      <option key={customer.id} value={index}>
+                        {customer.nameFantasy}
+                      </option>
+                    );
+                  })}
+                </select>
+              </label>
 
-              <label>About: </label>
-              <select value={about} onChange={handleAboutSelect}>
-                <option value="Support" key={1}>
-                  Support
-                </option>
-                <option value="Financial" key={2}>
-                  Financial
-                </option>
-                <option value="Technical Visit" key={3}>
-                  Technical visit
-                </option>
-              </select>
+              <label htmlFor="about">
+                About:
+                <select value={about} onChange={handleAboutSelect} id="about">
+                  <option value="Support">Support</option>
+                  <option value="Financial">Financial</option>
+                  <option value="Technical Visit">Technical visit</option>
+                </select>
+              </label>
 
               <Status>
                 <input
@@ -85,14 +143,17 @@ function New() {
               </Status>
             </FormLabel>
 
-            <label>Complement</label>
-            <textarea
-              cols="5"
-              rows="5"
-              placeholder="Describe the problem (optional)"
-              type="text"
-              onChange={(e) => setComplement(e.target.value)}
-            />
+            <label htmlFor="complement">
+              <p>Complement</p>
+              <textarea
+                cols="5"
+                rows="5"
+                placeholder="Describe the problem (optional)"
+                type="text"
+                onChange={(e) => setComplement(e.target.value)}
+                id="complement"
+              />
+            </label>
             <button type="submit">Save</button>
           </Form>
         </Content>
